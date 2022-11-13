749c4751b3796c39f10dfe78167985585b82ad4a
Ingrida425 <98320957+Ingrida425@users.noreply.github.com>
2022-11-13 18:35:30 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">There will be the Dictionary 📚</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer"> lalala Coded by Ingrida Kostiuk</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
