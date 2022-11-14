24c3077524ebd31cd024add0e8286f37569396b5
Ingrida425 <98320957+Ingrida425@users.noreply.github.com>
2022-11-14 20:47:16 +0100

    added axios , bootstrap and tarnsfered dictionary, meaning and phonetic

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+import Results from "./Results";
+import Photos from "./Photos";
+
 export default function Dictionary() {
-  return "Hello from dictionary";
+  const [keyword, setKeyword] = useState("sunshine");
+  const [definitions, setDefinitions] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
+
+  function handleResponse(response) {
+    setDefinitions(response.data[0]);
+  }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000019d453cb1917846e3bd2807ec419324df ";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeyword}
+              placeholder="Enter a word"
+            />
+          </form>
+        </section>
+        <Results results={definitions} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
